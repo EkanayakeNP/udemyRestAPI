5133e5ad769704f6003014405a2ccbbe74ab826e
EkanayakeNP <ekanayake65np@gmail.com>
2020-04-04 23:58:49 +0530

    initial commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.in28minutes.rest.webservices.restfulwebservices.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.in28minutes.rest.webservices.restfulwebservices.user.UserNotFoundException;
+
+@ControllerAdvice
+@Controller
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
+		
+		ExceptionRespose exceptionRespose = 
+				new ExceptionRespose(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity(exceptionRespose,HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+		
+		ExceptionRespose exceptionRespose = 
+				new ExceptionRespose(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity(exceptionRespose,HttpStatus.NOT_FOUND);
+	}
+}
